7277fbae764829aa8925a84e0fd0ce46ee518cf3
mxy493 <mxy20493@gmail.com>
2019-05-23 01:10:58 +0800

    写完最后几个函数，添加部分注释

--- StringFunc/StringFunc.cpp
+++ StringFunc/StringFunc.cpp
@@ -5,40 +5,37 @@
 #include <string>
 using namespace std;
 
-void strcpy1(char* p, const char* p1);					//复制字符串
-void strncpy1(char* p, const char* p1, int n);			//复制指定长度字符串
-void strcat1(char* p, char* p1);						//附加字符串
-void strncat1(char* p, const char* p1, int n);			//附加指定长度字符串
-int strlen1(const char* p);								//取字符串长度
-int strcmp1(const char* p, const char* p1);				//比较字符串
-int strcasecmp1(const char* p, const char* p1);			//忽略大小写比较字符串
-int strncmp1(const char* p, const char* p1, int n);		//比较指定长度字符串
-int strchr1(const char* p, char c);						//在字符串中查找指定字符
-int strrchr1(const char* p, char c);					//在字符串中反向查找
-int strstr1(const char* p, const char* p1);				//查找字符串
-double strtod1(const char* str, char* ppend);			//从字符串 str 中转换 double 类型数值，并将后续的字符串指针存储到 ppend 指向的 char* 类型存储。
+void strcpy1(char* str1, const char* str2);					//复制字符串
+void strncpy1(char* str1, const char* str2, int n);			//复制指定长度字符串
+void strcat1(char* str1, char* str2);						//附加字符串
+void strncat1(char* str1, const char* str2, int n);			//附加指定长度字符串
+int strlen1(const char* p);									//取字符串长度
+int strcmp1(const char* p, const char* p1);					//比较字符串
+int strcasecmp1(const char* p, const char* p1);				//忽略大小写比较字符串
+int strncmp1(const char* p, const char* p1, int n);			//比较指定长度字符串
+int strchr1(const char* p, char c);							//在字符串中查找指定字符
+int strrchr1(const char* p, char c);						//在字符串中反向查找
+int strstr1(const char* p, const char* p1);					//查找字符串
+double strtod1(const char* str, char* ppend);				//从字符串 str 中转换 double 类型数值，并将后续的字符串指针存储到 ppend 指向的 char* 类型存储。
 
-long strtol1(const char* str, char* ppend, const int base);	//从字符串 str 中转换 long 类型整型数值，base 显式设置转换的整型进制，设置为 0 以根据特定格式判断所用进制，0x, 0X 前缀以解释为十六进制格式整型，0    前缀以解释为八进制格式整型
+long strtol1(const char* str, char* ppend, const int base);	//从字符串 str 中转换 long 类型整型数值，base 显式设置转换的整型进制，设置为 0 以根据特定格式判断所用进制，0x, 0X 前缀以解释为十六进制格式整型，0前缀以解释为八进制格式整型
+
 int atoi1(const char* str);									//字符串转换到 int 整型
-float atof1(const char* str);								//字符串转换到 double 符点数
+double atof1(const char* str);								//字符串转换到 double 符点数
 long atol1(const char* str);								//字符串转换到 long 整型
 
 int main()
 {
-	char str1[100] = "Bsa0.24";
-	char str2[100] = "62";
-	//cout << "Input str1:";
-	//cin >> str1;
-	//cout << "Input str2:";
-	//cin >> str2;
+	char str1[100] = "24.2ddas";
+	char str2[100] = "6223";
 
 	//strcpy1(str1, str2);
 	//strncpy1(str1, str2, 2);
 	//strcat1(str1, str2);
 	//strncat1(str1, str2, 3);
 
-	cout << str1 << endl;
-	cout << str2 << endl;
+	cout << "str1:" << str1 << endl;
+	cout << "str2:" << str2 << endl;
 	//cout << strlen1(str1) << endl;
 	//cout << strlen1(str2) << endl;
 	//cout << strcmp1(str1, str2) << endl;
@@ -48,59 +45,62 @@ int main()
 	//cout << strrchr1(str1, '6') << endl;
 	//cout << strstr1(str1, str2) << endl;
 	//cout << strtod1(str1, str2) << endl;
+	//cout << atoi1(str1) << endl;
+	//cout << atof1(str1) << endl;
+	//cout << atol1(str1) << endl;
 
-	std::cout << "Hello World!\n" << endl;
 	return 0;
 }
 
-void strcpy1(char* p, const char* p1)
+void strcpy1(char* str1, const char* str2)
 {
-	if ((p == NULL) && (p1 == NULL))
+	if ((str1 == NULL) || (str2 == NULL))		//判断指针是否为空，为空退出函数
 		return;
-	while (p && (*p++ = *p1++) != '\0')
+	while (str1 && (*str1++ = *str2++) != '\0')	//str1非空，并且*str2赋值给*str1，str1、str2后移一位
 		NULL;
 }
 
-void strncpy1(char* p, const char* p1, int n)
+void strncpy1(char* str1, const char* str2, int n)
 {
-	if ((p == NULL) && (p1 == NULL))
+	if ((str1 == NULL) || (str2 == NULL))
 		return;
-	while (p && n--)
+	while (n--)					//逐个字符复制，复制n个字符
 	{
-		if (*p1 != '\0')
+		if (*str2 != '\0')
 		{
-			*p++ = *p1++;
+			*str1++ = *str2++;	//*str2赋值给*str1，并且str1、str2后移一位
 		}
 		else
 		{
-			*p++ = '\0';
+			*str1++ = '\0';		//str1补'\0'
 		}
 	}
-	while (p)
+	while (str1)
 	{
-		if (*p != '\0')
-			* p++ = '\0';
+		if (*str1 != '\0')		//从str1[n]开始将其后所有字符置为'\0'
+			* str1++ = '\0';
+		else break;				//遇'\0'表明字符串结束
 	}
 }
 
-void strcat1(char* p, char* p1)
+void strcat1(char* str1, char* str2)
 {
-	if((p == NULL) && (p1 == NULL))
+	if((str1 == NULL) || (str2 == NULL))
 		return;
-	while (p && *p != '\0')
-		++p;
-	while (p && (*p++ = *p1++) != '\0')
+	while (*str1 != '\0')				//将指针str1后移使其指向字符串str1尾字符后一位
+		++str1;
+	while ((*str1++ = *str2++) != '\0')	//逐个复制字符
 		NULL;
 }
 
-void strncat1(char* p, const char* p1, int n)
+void strncat1(char* str1, const char* str2, int n)
 {
-	if ((p == NULL) && (p1 == NULL))
+	if ((str1 == NULL) && (str2 == NULL))
 		return;
-	while (p && *p != '\0')
-		++p;
-	while (p && n-- && *p1 != '\0')
-		* p++ = *p1++;
+	while (*str1 != '\0')				//将指针str1后移使其指向字符串str1尾字符后一位
+		++str1;
+	while (n-- && *str2 != '\0')		//逐个复制n个字符
+		* str1++ = *str2++;
 }
 
 int strlen1(const char* p)
@@ -294,18 +294,134 @@ double strtod1(const char* str, char* ppend)
 
 long strtol1(const char* str, char* ppend, const int base)
 {
+	if (str == NULL)
+		return NULL;
+	long ret;
+	int inte = 0;			//全部转为十进制整数
+	bool find = false;
+	//for (int i = 0; i < s.size(); i++) 
+	//{
+	//	char t = s[i];
+	//	if (t >= '0' && t <= '9')
+	//		inte = inte * base + t - '0';
+	//	else inte = inte * base + t - 'a' + 10;
+	//}
+	
+	if (base == 2)
+	{
+		while (*str != '\0')
+		{
+			if (*str >= '0' && *str <= '1')
+			{
+				find = true;
+				inte = inte * 10 + (*str - '0');
+			}
+			else if (find == true)
+				break;
+		}
+
+	}
+	else if (base == 8)
+	{
+
+	}
+	else if (base == 10)
+	{
+
+	}
+	else if (base == 16)
+	{
+
+	}
 	return 0;
 }
 
 int atoi1(const char* str)
 {
-	return 0;
+	if (str == NULL)
+		return NULL;
+	int ret = 0;
+	bool find = false;
+	while (*str != '\0')
+	{
+		if (*str >= '0' && *str <= '9')
+		{
+			find = true;
+			ret = ret * 10 + *str - '0';
+		}
+		else if (find == true)
+			return ret;
+		str++;
+	}
+	if (find == true)
+		return ret;
+	else
+		return NULL;
 }
 
-float atof1(const char* p)
+double atof1(const char* str)
 {
+	if (str == NULL)
+		return NULL;
+	double d;
+	int deci = 0;
+	bool find = false;
+	while (*str != '\0')
+	{
+		if (*str == '.' && deci == 0)
+		{
+			++deci;
+			str++;
+			continue;
+		}
+		else if (*str >= '0' && *str <= '9')
+		{
+			if (find == false)
+			{
+				find = true;
+				d = 0;
+			}
+			if (deci == 0)
+			{
+				d = d * 10 + ((int)* str - 48);
+				str++;
+				continue;
+			}
+			else
+			{
+				d = d + ((int)* str - 48) / pow(10, deci);
+				++deci;
+			}
+		}
+		else if (find)
+			return d;
+		str++;
+	}
+	if (find)
+		return d;
+	else
+		return NULL;
 }
 
 long atol1(const char* str)
 {
+	if (str == NULL)
+		return NULL;
+	long ret = 0;
+	bool find = false;
+	while (*str != '\0')
+	{
+		if (*str >= '0' && *str <= '9')
+		{
+			find = true;
+			ret = ret * 10 + *str - '0';
+		}
+		else if (find == true)
+			return ret;
+		str++;
+	}
+	if (find == true)
+		return ret;
+	else
+		return NULL;
 }
